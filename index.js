315b886de3d68c8e225260f640492412ff4b8bf4
Jeffrey Paulino <jeffreypaulino@gmail.com>
2019-03-16 00:55:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,6 +12,18 @@ function whisper(string) {
 function logShout(string) {
   console.log(shout(string));
 }
+
+function logWhisper(string) {
+  console.log(whisper(string));
+}
+
+function sayHiToGrandma(string) {
+  if (string == string.toLowerCase) {
+    return "I can't hear you!";
+  }
+}
+
+
 /*
 console.log(shout());
 console.log(whisper());
